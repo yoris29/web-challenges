28aa30f575edb91e3c4f0cb6c5f1c5513ee95be2
Adel2411 <hadjarabadel.2411@gmail.com>
2025-06-27 01:53:55 +0100

    Added tailwind css to the project frontend folder

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -2,8 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-semibold">Hello World!</h1>
     </div>
   );
 }
