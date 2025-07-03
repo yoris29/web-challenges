5b6dd76a7c719438bd30e14c86651e89236a6180
yoris29 <ydoumi29@gmail.com>
2025-07-03 16:22:20 +0100

    async function start listening to requests only when connected to prisma

--- project/backend/src/app.js
+++ project/backend/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import errorHandling from "./middleware/errorHandling.js";
 import notesRouter from "./routes/notes.js";
+import prisma from "./prisma-client.js";
 dotenv.config();
 
 const app = express();
@@ -12,7 +13,16 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/notes", notesRouter);
 app.use(errorHandling);
 
-const port = Number(process.env.PORT) || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on localhost:${port}`);
-});
+const server = async () => {
+  try {
+    await prisma.$connect();
+    const port = Number(process.env.PORT) || 5000;
+    app.listen(port, () => {
+      console.log(`Server is running on localhost:${port}`);
+    });
+  } catch (err) {
+    console.error("DB connection failed", err);
+    process.exit(1);
+  }
+};
+server();
